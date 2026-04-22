e546f1df65dfaa93bbd81a8e83073ddcf3cf3efd
vanilson <vanilson.liins@gmail.com>
2026-04-22 14:59:03 -0300

    Ajustanto caixa de texto 2

--- test_list.js
+++
@@ -1,7 +0,0 @@
-async function test() {
-  const KEY = "AIzaSyAZxQPV-7GwZYz_GguAZ-jT7u3KsyAoFYg";
-  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${KEY}`);
-  const data = await response.json();
-  console.log(JSON.stringify(data, null, 2));
-}
-test();
--- test_list2.js
+++
@@ -1,8 +0,0 @@
-async function test() {
-  const KEY = "AIzaSyAZxQPV-7GwZYz_GguAZ-jT7u3KsyAoFYg";
-  const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${KEY}`);
-  const data = await response.json();
-  const names = data.models.map(m => m.name.replace("models/", ""));
-  console.log("AVAILABLE MODELS:", names.filter(n => n.includes("gemini")));
-}
-test();
